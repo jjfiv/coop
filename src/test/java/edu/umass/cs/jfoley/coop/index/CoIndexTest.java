1b3aba8d82ad8dbdec3c52111d38073aea28cdd4
John Foley <jfoley@cs.umass.edu>
2015-06-03 13:12:32 -0400

    Leave VocabIndex behind

--- src/test/java/edu/umass/cs/jfoley/coop/index/CoIndexTest.java
+++ src/test/java/edu/umass/cs/jfoley/coop/index/CoIndexTest.java
@@ -18,42 +18,6 @@ import java.util.List;
  */
 public class CoIndexTest {
 
-
-  @Test
-  public void testCoIndex2() throws IOException, InterruptedException {
-    List<String> docs = new ArrayList<>(Arrays.asList(IO.resource("/gettysburg_address.txt").split("\n+")));
-
-    try (TemporaryDirectory tmpdir = new TemporaryDirectory()) {
-      try (VocabBuilder builder = new VocabBuilder(tmpdir)) {
-        for (int i = 0; i < docs.size(); i++) {
-          String doc = docs.get(i);
-          builder.addDocument(String.format("ga.p%d", i), doc);
-        }
-      }
-
-      System.err.println(tmpdir.children());
-      Spawn.doProcess("/bin/ls", tmpdir.getPath(), "-ltr");
-
-      try (VocabReader reader = new VocabReader(tmpdir)) {
-        System.err.println(IterableFns.intoList(reader.names.reverseReader.keys()));
-        System.err.println(IterableFns.intoList(reader.vocab.reverseReader.keys()));
-
-        for (String term : reader.vocab.reverseReader.keys()) {
-          PostingMover<PositionsList> pf = reader.getPositionsMover(term);
-          int totalCount = 0;
-          for(; !pf.isDone(); pf.next()) {
-            int count = pf.getCurrentPosting().size();
-            totalCount+=count;
-          }
-
-          if(totalCount > 1) {
-            System.err.println(term + " " + totalCount);
-          }
-        }
-      }
-    }
-  }
-
   @Test
   public void testCoIndex3() throws IOException, InterruptedException {
     List<String> docs = new ArrayList<>(Arrays.asList(IO.resource("/gettysburg_address.txt").split("\n+")));
