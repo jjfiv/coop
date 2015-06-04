b29f041d255f93519a891ec1837512f418ca03e0
John Foley <jfoley@cs.umass.edu>
2015-06-04 13:32:09 -0400

    Get distracted by the idea of chaining SinkFn<T> may have just rediscovered Tupleflow-like graph execution.

--- src/main/java/edu/umass/cs/jfoley/coop/querying/eval/QueryEvalEngine.java
+++ src/main/java/edu/umass/cs/jfoley/coop/querying/eval/QueryEvalEngine.java
@@ -25,27 +25,48 @@ public class QueryEvalEngine {
    * @param <T> the type of result expected from the query.
    */
   public static <T> void Evaluate(Mover mover, QueryEvalNode<T> query, SinkFn<DocumentResult<T>> output) {
-    for(; mover.hasNext(); mover.next()) {
-      int document = mover.currentKey();
+    mover.execute(new DocumentQueryEvaluator<>(query, output));
+  }
+
+  public static <T> void EvaluateOneToMany(Mover mover, QueryEvalNode<? extends Collection<T>> query, SinkFn<DocumentResult<T>> output) {
+    mover.execute(new DocumentToManyQueryEvaluator<>(query, output));
+  }
+
+  public static class DocumentQueryEvaluator<T> implements SinkFn<Integer> {
+    private final QueryEvalNode<T> query;
+    private final SinkFn<DocumentResult<T>> output;
+
+    public DocumentQueryEvaluator(QueryEvalNode<T> query, SinkFn<DocumentResult<T>> output) {
+      this.query = query;
+      this.output = output;
+    }
+
+    @Override
+    public void process(Integer document) {
       T value = query.calculate(document);
-      if(value != null) {
+      if (value != null) {
         output.process(new DocumentResult<>(document, value));
       }
     }
   }
+  public static class DocumentToManyQueryEvaluator<T> implements SinkFn<Integer> {
+    private final QueryEvalNode<? extends Collection<T>> query;
+    private final SinkFn<DocumentResult<T>> output;
 
-  public static <T> void EvaluateOneToMany(Mover mover, QueryEvalNode<? extends Collection<T>> query, SinkFn<DocumentResult<T>> output) {
-    for(; mover.hasNext(); mover.next()) {
-      int document = mover.currentKey();
+    public DocumentToManyQueryEvaluator(QueryEvalNode<? extends Collection<T>> query, SinkFn<DocumentResult<T>> output) {
+      this.query = query;
+      this.output = output;
+    }
+
+    @Override
+    public void process(Integer document) {
       Collection<T> values = query.calculate(document);
-      if(values == null || values.isEmpty()) {
-        continue;
+      if (values == null || values.isEmpty()) {
+        return;
       }
       for (T value : values) {
         output.process(new DocumentResult<>(document, value));
       }
     }
   }
-
-
 }
