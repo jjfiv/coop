990b2d67bb6237d35528b56eed5b93a92b4d6828
John Foley <jfoley@cs.umass.edu>
2015-06-18 13:47:49 -0400

    use server to hover over things

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -19,7 +19,28 @@ function standardErrorHandler(err) {
 
 $(function() {
     postJSON("/api/randomSentences", {}, function(response){
+        var html = '';
+        html += 'Finding these sentences took: '+response.time+'ms.';
+        html += '<ul>';
         console.log(response);
+
+        _.forEach(response.sentences, function(s) {
+            html += '<li>';
+            html += '<span class="id">'+ s[0].sentenceId + '</span> ';
+
+            _.forEach(s, function(token) {
+                var title = _(token.terms).map(function(v, k) {
+                    if(k.contains("ner") && v == "O") return "";
+                    return k+"="+v;
+                }).reject(_.isEmpty).join('; ');
+
+                html += '<span title="'+title+'" class="token">'+ token.terms.true_terms + '</span> ';
+            });
+            html += '</li>';
+        });
+
+        html += '</ul>';
+        $('#sentences').html(html);
     });
 });
 
