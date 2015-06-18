fb2c65075d732e6f7877d352d6c7f1d2fa58a746
John Foley <jfoley@cs.umass.edu>
2015-06-18 16:06:24 -0400

    Reformat and add note

--- src/main/resources/events.js
+++ src/main/resources/events.js
@@ -5,23 +5,24 @@ function createEventBus() {
     var event_bus = {};
 
     event_bus.register= function(event_string, listener) {
-            var old_listeners = _.get(mapped_events, event_string, []);
-            old_listeners.push(listener);
-            mapped_events[event_string] = old_listeners;
-        };
+        var old_listeners = _.get(mapped_events, event_string, []);
+        old_listeners.push(listener);
+        mapped_events[event_string] = old_listeners;
+    };
+    // Note, I'm not sure this function works at all.
     event_bus.unregister= function(event_string, listener) {
         mapped_events[event_string] = _.without(mapped_events[event_string], listener);
     };
     event_bus.signal= function(event_string, props) {
-            _.forEach(_.get(mapped_events, event_string, []), function(x) {
-                x.handleSignal(event_string, props);
-            });
-        };
+        _.forEach(_.get(mapped_events, event_string, []), function(x) {
+            x.handleSignal(event_string, props);
+        });
+    };
     event_bus.async_signal= function (event_string, props) {
-            _.defer(function() {
-                signal(event_string, props);
-            })
-        };
-    event_bus.debounced_async_signal= _.debounce(event_bus.async_signal.bind(event_bus), 50)
+        _.defer(function() {
+            signal(event_string, props);
+        })
+    };
+    event_bus.debounced_async_signal= _.debounce(event_bus.async_signal.bind(event_bus), 50);
     return event_bus;
 }
