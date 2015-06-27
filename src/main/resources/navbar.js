1bb2efd3e5f7ee9595d9b132cf7c8fa38858c3d3
John Foley <jfoley@cs.umass.edu>
2015-06-26 22:26:18 -0400

    Last checkin before babel

---
+++ src/main/resources/navbar.js
@@ -0,0 +1,51 @@
+function makeClasses(classList) {
+    return _.reduce(classList, function (a, b) {
+        return a + " " + b;
+    });
+}
+
+var NavBar = React.createClass({
+    componentWillMount: function() {
+
+    },
+    render: function() {
+        console.log(this.props.links);
+        var linkElems = _(this.props.links).map(function(link) {
+            var url = window.location.href;
+            var host = window.location.host;
+            var offset = url.indexOf(host);
+
+            console.log(url.substr(offset + host.length));
+            return <a key={link.name} className={"nav"} href={_.first(link.url)}>{link.name}</a>
+        }).value();
+        console.log(linkElems);
+
+        linkElems.push(
+            <input key={"search"} className={"rightNav"} id={"searchBox"} type={"text"} title={"Search Sentences"} placeholder={"Search Sentences"} />
+        );
+
+        return <div id={"header"}>{linkElems}</div>;
+    }
+});
+
+$(function() {
+    var links = [
+        { name: "Home", "url": ["/", "/index.html"] },
+        { name: "Search", "url": "/search.html" }
+    ];
+
+    React.render(<NavBar links={links} />, document.getElementById("top"));
+});
+
+/*<div id="header">
+    <a class="nav" href="#">Home</a>
+    <a class="nav" href="#">Search</a>
+    <a class="nav" href="#">Explore</a>
+    <a class="active nav" href="#">Labels</a>
+</div>
+<div id="subheader">
+    <a class="subnav" href="#">Label Most Likely</a>
+    <a class="subnav" href="#">Label Most Confusing</a>
+<a class="subnav" href="#">Label Least Likely</a>
+<a class="subnav active" href="#">Label Randomly</a>
+</div>*/
