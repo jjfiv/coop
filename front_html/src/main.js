e5a6868d4527ca743d80c8313ca44ad72b291928
John Foley <jfoley@cs.umass.edu>
2015-08-06 12:39:11 -0400

    Shared links to a single place

--- front_html/src/main.js
+++ front_html/src/main.js
@@ -1,14 +1,36 @@
+class Navigation extends React.Component {
+    render() {
+        let pages = {
+            "index.html": "Phrase Search",
+            "docSearch.html": "Document Search"
+        };
+
+        let links = _(pages).map((val, key) => {
+            return <a className="nav" key={key} href={key}>{val}</a>;
+        }).value();
+
+        return <span>{links}</span>;
+    }
+}
+Navigation.propTypes = {};
+
 class Main {
+    static navigation() {
+        React.render(<Navigation />,document.getElementById("header"));
+    }
     static render(what) {
         React.render(what,document.getElementById("content"));
     }
     static index() {
+        Main.navigation();
         Main.render(<PhraseSearchInterface />);
     }
     static docs() {
+        Main.navigation();
         Main.render(<DocSearchInterface />);
     }
     static doc() {
+        Main.navigation();
         Main.render(<DocViewInterface urlParams={getURLParams()} />);
     }
 }
