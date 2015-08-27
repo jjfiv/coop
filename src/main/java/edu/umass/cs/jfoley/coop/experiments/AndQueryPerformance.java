3c1ccc6bf0a1e097c7304279dc933ac4e6f958e1
John Foley <jfoley@cs.umass.edu>
2015-08-27 17:11:19 -0400

    use a for loop rather than the lambda

--- src/main/java/edu/umass/cs/jfoley/coop/experiments/AndQueryPerformance.java
+++ src/main/java/edu/umass/cs/jfoley/coop/experiments/AndQueryPerformance.java
@@ -34,41 +34,30 @@ public class AndQueryPerformance {
     int blockSize;
     IntsCoder docsCoder;
 
-//AND Results: {total=2586000.0, min=1724.0, variance=0.0, max=1724.0, mean=1724.0, count=1500.0, stddev=0.0}
-//OR Results: {total=5796000.0, min=3864.0, variance=0.0, max=3864.0, mean=3864.0, count=1500.0, stddev=0.0}
     switch (what) {
-//Time to compute AND: {total=27.621801673999997, min=0.01757542, variance=6.4521113543075415E-6, max=0.066615237, mean=0.018414534449333306, count=1500.0, stddev=0.0025401006583022534}
-     //Time to compute AND.pull: {total=197.41887378799953, min=0.125967717, variance=4.006355699884853E-5, max=0.228630896, mean=0.13161258252533314, count=1500.0, stddev=0.006329577947924216}
       case "positions":
         blockSize = 128;
         docsCoder = new DeltaIntListCoder();
         break;
 
-      //Time to compute AND: {total=26.811990208999983, min=0.017078665, max=0.083336225, variance=8.685144205738402E-6, mean=0.017874660139333358, count=1500.0, stddev=0.0029470568718194773}
-     // Time to compute OR: {total=45.359020480000055, min=0.028875015, variance=1.4112820130411703E-5, max=0.10346278, mean=0.030239346986666676, count=1500.0, stddev=0.0037567033593846216}
-      //Time to compute AND.pull: {total=66.83807683900001, min=0.129580105, variance=5.7199459874298295E-5, max=0.215042732, mean=0.13367615367800004, count=500.0, stddev=0.007563032452283826}
+      //Time to compute AND.pull: {total=138.21037495599998, min=0.259082729, variance=0.0014720888876883459, max=1.065861554, mean=0.2764207499120004, count=500.0, stddev=0.0383678105667804}
       case "upositions":
         blockSize = 128;
         docsCoder = new DeltaIntListCoder(VarUInt.instance, VarUInt.instance);
         break;
 
-     //Time to compute AND: {total=26.96598929000001, min=0.016934723, max=0.215823924, variance=4.119278375674504E-5, mean=0.017977326193333334, count=1500.0, stddev=0.006418160465175753}
-     // Time to compute OR: {total=45.02783459400002, min=0.028765423, variance=2.814424523583346E-5, max=0.138144068, mean=0.03001855639599999, count=1500.0, stddev=0.005305115006843251}
-     // Time to compute AND.pull: {total=68.92293945799997, min=0.13206452, max=0.272646274, variance=8.875294364984844E-5, mean=0.13784587891600006, count=500.0, stddev=0.009420878072125148}
+//Time to compute AND.pull: {total=135.042183165, min=0.258258485, max=0.494070098, variance=2.0519558900570072E-4, mean=0.2700843663300001, count=500.0, stddev=0.014324649699231766}
+//Results: {total=7.83225E7, min=156645.0, variance=0.0, max=156645.0, mean=156645.0, count=500.0, stddev=0.0}
+//Sizes: {total=3.7155275E9, min=1.0, variance=125.23970931428381, max=405.0, mean=7.906471107711957, count=4.69935E8, stddev=11.191054879424183}
       case "p256":
         blockSize = 256;
         docsCoder = new DeltaIntListCoder(VarUInt.instance, VarUInt.instance);
         break;
-     // Time to compute AND: {total=27.468614638999995, min=0.017179428, variance=2.6837262732128046E-5, max=0.159000902, mean=0.01831240975933336, count=1500.0, stddev=0.005180469354424177}
-      //Time to compute OR: {total=45.43383761899991, min=0.028832518, variance=1.4959066513673269E-5, max=0.107375356, mean=0.030289225079333303, count=1500.0, stddev=0.0038676952457081296}
       case "p512":
         blockSize = 512;
         docsCoder = new DeltaIntListCoder(VarUInt.instance, VarUInt.instance);
         break;
 
-      // Time to compute AND: {total=18.22282244800001, min=0.011085718, max=0.180091519, variance=4.288220095037301E-5, mean=0.01214854829866666, count=1500.0, stddev=0.0065484502708941}
-      // Time to compute OR: {total=45.65927288399996, min=0.028864116, max=0.296642395, variance=6.11430209632479E-5, mean=0.03043951525600003, count=1500.0, stddev=0.007819400294347892}
-      // Time to compute AND.pull: {total=204.4864220810002, min=0.130118374, variance=5.888853208308047E-5, max=0.341883195, mean=0.13632428138733327, count=1500.0, stddev=0.007673886374131459}
       case "p8":
         blockSize = 1;
         docsCoder = new DeltaIntListCoder(VarUInt.instance, VarUInt.instance);
@@ -94,7 +83,7 @@ public class AndQueryPerformance {
     Parameters argp = Arguments.parse(args);
     Directory input = Directory.Read(argp.get("input", "robust.ints"));
 
-    String target = "p256";
+    String target = "upositions";
     PostingIndex.PostingsConfig<Integer,PositionsIndexFile.PositionsCountMetadata,PositionsList> cfg = getCfg(target);
     IOMap<Integer, PostingMover<PositionsList>> index = cfg.openReader(input, target);
     IdMaps.Reader<String> vocab = GalagoIO.openIdMapsReader(input.childPath("vocab"), FixedSize.ints, CharsetCoders.utf8);
@@ -147,17 +136,17 @@ public class AndQueryPerformance {
     for (int k = 0; k < 500; k++) {
       highestNums.clear();
       startMovement = System.nanoTime();
-      andMover.execute((x) -> {
+      for(andMover.start(); !andMover.isDone(); andMover.next()) {
+        int x = andMover.currentKey();
         highestNums.add(x);
         for (PostingMover<PositionsList> iter : iters) {
           sizes.push((double) iter.getCurrentPosting().size());
         }
-      });
+      }
       andMover.reset();
       endMovement = System.nanoTime();
       performance.push((endMovement - startMovement) / 1e9);
       results.push(highestNums.total());
-
     }
     System.out.println("Time to compute AND.pull: " + performance);
     System.out.println("Results: "+results);
