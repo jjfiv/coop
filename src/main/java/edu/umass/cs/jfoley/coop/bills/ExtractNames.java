bc42d0d44afe85b150ac3e07111c7509ab0c00c3
John Foley <jfoley@cs.umass.edu>
2015-09-16 18:13:52 -0400

    ExtractNames.java - entity tagging about 800 docs / s per core

--- src/main/java/edu/umass/cs/jfoley/coop/bills/ExtractNames.java
+++ src/main/java/edu/umass/cs/jfoley/coop/bills/ExtractNames.java
@@ -2,10 +2,12 @@ package edu.umass.cs.jfoley.coop.bills;
 
 import ciir.jfoley.chai.IntMath;
 import ciir.jfoley.chai.collections.Pair;
+import ciir.jfoley.chai.collections.list.FixedSlidingWindow;
 import ciir.jfoley.chai.collections.list.IntList;
 import ciir.jfoley.chai.collections.util.ListFns;
 import ciir.jfoley.chai.io.Directory;
 import ciir.jfoley.chai.io.IO;
+import ciir.jfoley.chai.math.StreamingStats;
 import ciir.jfoley.chai.time.Debouncer;
 import gnu.trove.map.hash.TObjectIntHashMap;
 import org.lemurproject.galago.core.parse.TagTokenizer;
@@ -33,7 +35,7 @@ public class ExtractNames {
 
     Debouncer msg = new Debouncer(500);
     int N = 20;
-    ArrayList<HashSet<IntList>> matchingBySize = new ArrayList<>(N);
+    ArrayList<HashSet<List<Integer>>> matchingBySize = new ArrayList<>(N);
     for (int i = 0; i < N; i++) { matchingBySize.add(new HashSet<>()); }
 
     long start = System.currentTimeMillis();
@@ -43,13 +45,13 @@ public class ExtractNames {
     }
     long end = System.currentTimeMillis();
     System.err.println("# preload vocab: "+(end-start)+"ms.");
-    int i = 0;
+    int docNameIndex = 0;
     for (String name : index.names.values()) {
-      i++;
+      docNameIndex++;
       String text = name.replace('_', ' ');
       List<String> query = tokenizer.tokenize(text).terms;
       int size = query.size();
-      if(size == 0 || size >= N) continue;
+      if(size == 0 || size > N) continue;
       IntList qIds = new IntList(query.size());
       for (String str : query) {
         int tid = vocabLookup.get(str);
@@ -68,7 +70,7 @@ public class ExtractNames {
         System.out.println(text);
         System.out.println(query);
         System.out.println(qIds);
-        System.out.println(msg.estimate(i, count));
+        System.out.println(msg.estimate(docNameIndex, count));
         System.out.println(ListFns.map(matchingBySize, HashSet::size));
       }
     }
@@ -76,13 +78,66 @@ public class ExtractNames {
     System.out.println(ListFns.map(matchingBySize, HashSet::size));
 
     try (PrintWriter entities = IO.openPrintWriter(target.baseDir.childPath("dbpedia.titles.intq.gz"))) {
-      for (HashSet<IntList> intLists : matchingBySize) {
-        for (IntList intList : intLists) {
-          for (int ti = 0; ti < intList.size(); ti++) {
-            entities.print(intList.getQuick(ti));
+      for (HashSet<List<Integer>> intLists : matchingBySize) {
+        for (List<Integer> intList : intLists) {
+          for (Integer x : intList) {
+            entities.print(x);
             entities.print(' ');
           }
           entities.println();
+        }
+      }
+    }
+
+    ArrayList<FixedSlidingWindow<Integer>> patternBuffers = new ArrayList<>();
+    for (int i = 0; i < N; i++) {
+      patternBuffers.add(new FixedSlidingWindow<>(i+1));
+    }
+
+    Debouncer msg2 = new Debouncer(500);
+    StreamingStats hitsPerDoc = new StreamingStats();
+    long globalPosition = 0;
+    try (PrintWriter hits = IO.openPrintWriter(target.baseDir.childPath("dbpedia.titles.hits.gz"))) {
+      int ND = target.corpus.numberOfDocuments();
+      for (int docIndex = 133367; docIndex < ND; docIndex++) {
+        int[] doc = target.corpus.getDocument(docIndex);
+
+        int hitcount = 0;
+        for (int position = 0; position < doc.length; position++) {
+          int term = doc[position];
+
+          for (int i = 0; i < patternBuffers.size(); i++) {
+            FixedSlidingWindow<Integer> buffer = patternBuffers.get(i);
+            buffer.add(term);
+            if (buffer.full() && matchingBySize.get(i).contains(buffer)) {
+              int hitStart = position-i;
+              int hitSize = i+1;
+              // hit!
+              //hits.printf("%d\t%d\t%d\t%s\n", docIndex, position-i, i+1, buffer);
+              hits.print(docIndex); hits.print('\t');
+              hits.print(hitStart); hits.print('\t');
+              hits.print(hitSize); hits.print('\t');
+
+              assert(hitStart >= 0);
+              assert(hitSize >= 1);
+
+              for (int termIndex = 0; termIndex < hitSize; termIndex++) {
+                hits.print(doc[termIndex+hitStart]); hits.print(' ');
+              }
+              hits.println();
+
+              hitcount++;
+            }
+          }
+        }
+        globalPosition += doc.length;
+        hitsPerDoc.push(hitcount);
+
+        if(msg2.ready()) {
+          System.out.println("docIndex="+docIndex);
+          System.out.println("NERing documents at: "+msg2.estimate(docIndex, ND));
+          System.out.println("NERing documents at terms/s: "+msg2.estimate(globalPosition));
+          System.out.println("hitsPerDoc: "+hitsPerDoc);
         }
       }
     }
