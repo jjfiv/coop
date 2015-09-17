9a344deefafe10625eb59f8de84159887a9c0606
John Foley <jfoley@cs.umass.edu>
2015-09-17 12:48:04 -0400

    Refactor PhraseDetector; callback version just as fast

--- src/main/java/edu/umass/cs/jfoley/coop/bills/ExtractNames2.java
+++ src/main/java/edu/umass/cs/jfoley/coop/bills/ExtractNames2.java
@@ -1,19 +1,13 @@
 package edu.umass.cs.jfoley.coop.bills;
 
-import ciir.jfoley.chai.collections.list.CircularIntBuffer;
-import ciir.jfoley.chai.collections.list.IntList;
 import ciir.jfoley.chai.io.Directory;
 import ciir.jfoley.chai.io.IO;
-import ciir.jfoley.chai.io.LinesIterable;
 import ciir.jfoley.chai.math.StreamingStats;
 import ciir.jfoley.chai.time.Debouncer;
+import edu.umass.cs.jfoley.coop.phrases.PhraseDetector;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.regex.Pattern;
 
 /**
  * @author jfoley
@@ -25,78 +19,40 @@ public class ExtractNames2 {
 
     long start, end;
 
-    int N = 20;
-    ArrayList<HashSet<List<Integer>>> matchingBySize = new ArrayList<>(N);
-    for (int i = 0; i < N; i++) { matchingBySize.add(new HashSet<>(60000)); }
-
     // load up precomputed queries:
-    Pattern spaces = Pattern.compile("\\s+");
     start = System.currentTimeMillis();
-    try (LinesIterable lines = LinesIterable.fromFile(target.baseDir.child("dbpedia.titles.intq.gz"))) {
-      for (String line : lines) {
-        String[] col = spaces.split(line);
-        int n = col.length;
-        if(n > N) continue;
-        IntList pattern = new IntList(n);
-        for (String str : col) {
-          pattern.add(Integer.parseInt(str));
-        }
-        matchingBySize.get(n - 1).add(pattern);
-      }
-    }
+    PhraseDetector detector = PhraseDetector.loadFromTextFile(20, target.baseDir.child("dbpedia.titles.intq.gz"));
     end = System.currentTimeMillis();
     System.out.println("loading titles: "+(end-start)+"ms.");
-
-
-    ArrayList<CircularIntBuffer> patternBuffers = new ArrayList<>(N);
-    for (int i = 0; i < N; i++) {
-      patternBuffers.add(new CircularIntBuffer(i+1));
-    }
 
     Debouncer msg2 = new Debouncer(500);
     StreamingStats hitsPerDoc = new StreamingStats();
     long globalPosition = 0;
-    try (PrintWriter hits = IO.openPrintWriter(target.baseDir.childPath("dbpedia.titles.hits.gz"))) {
+    try (PrintWriter hits = IO.openPrintWriter(target.baseDir.childPath("dbpedia.titles.hits.2.gz"))) {
       int ND = target.corpus.numberOfDocuments();
       for (int docIndex = 0; docIndex < ND; docIndex++) {
         int[] doc = target.corpus.getDocument(docIndex);
+        final int docId = docIndex;
 
         StringBuilder outputForThisDoc = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-          CircularIntBuffer buffer = patternBuffers.get(i);
-          buffer.clear();
-        }
-        int hitcount = 0;
-        for (int position = 0; position < doc.length; position++) {
-          int term = doc[position];
+        int hitcount = detector.match(doc, (hitStart, hitSize) -> {
+          //hits.printf("%d\t%d\t%d\t%s\n", docIndex, position-i, i+1, buffer);
+          outputForThisDoc
+              .append(docId).append('\t')
+              .append(hitStart).append('\t')
+              .append(hitSize).append('\t');
 
-          for (int i = 0; i < N; i++) {
-            CircularIntBuffer buffer = patternBuffers.get(i);
-            buffer.add(term);
-            if (buffer.full() && matchingBySize.get(i).contains(buffer)) {
-              int hitStart = position-i;
-              int hitSize = i+1;
+          assert(hitStart >= 0);
+          assert(hitSize >= 1);
 
-              //hits.printf("%d\t%d\t%d\t%s\n", docIndex, position-i, i+1, buffer);
-              outputForThisDoc
-                  .append(docIndex).append('\t')
-                  .append(hitStart).append('\t')
-                  .append(hitSize).append('\t');
-
-              assert(hitStart >= 0);
-              assert(hitSize >= 1);
-
-              for (int termIndex = 0; termIndex < hitSize; termIndex++) {
-                outputForThisDoc
-                    .append(doc[termIndex+hitStart])
-                    .append(' ');
-              }
-              outputForThisDoc.append('\n');
-
-              hitcount++;
-            }
+          for (int termIndex = 0; termIndex < hitSize; termIndex++) {
+            outputForThisDoc
+                .append(doc[termIndex+hitStart])
+                .append(' ');
           }
-        }
+          outputForThisDoc.append('\n');
+        });
+
         hits.print(outputForThisDoc.toString());
         globalPosition += doc.length;
         hitsPerDoc.push(hitcount);
---
+++ src/main/java/edu/umass/cs/jfoley/coop/phrases/PhraseDetector.java
@@ -0,0 +1,90 @@
+package edu.umass.cs.jfoley.coop.phrases;
+
+import ciir.jfoley.chai.collections.list.CircularIntBuffer;
+import ciir.jfoley.chai.collections.list.IntList;
+import ciir.jfoley.chai.io.LinesIterable;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * Contains an hashset for each candidate size while working through patterns that may match.
+ * This allows rapid tagging of a large dictionary. (300k+ terms/second with 355k dbpedia titles.)
+ * @author jfoley
+ */
+public class PhraseDetector {
+  public ArrayList<HashSet<List<Integer>>> matchingBySize;
+  public int N;
+  IntList matchingSizes;
+
+  public PhraseDetector(int N) {
+    this.N = N;
+    this.matchingBySize = new ArrayList<>();
+    for (int i = 0; i < N; i++) {
+      matchingBySize.add(new HashSet<>());
+    }
+    matchingSizes = new IntList(N);
+  }
+
+  public void addPattern(IntList data) {
+    int n = data.size()-1;
+    if(n < 0 || n >= N) return;
+    matchingBySize.get(n).add(data);
+  }
+
+  public boolean matches(List<Integer> query) {
+    int n = query.size()-1;
+    if(n < 0 || n >= N) return false;
+    return matchingBySize.get(n).contains(query);
+  }
+
+  public interface PhraseMatchListener {
+    void onPhraseMatch(int position, int size);
+  }
+
+  public int match(int[] data, PhraseMatchListener handler) {
+    // create circular buffers for this document:
+    ArrayList<CircularIntBuffer> patternBuffers = new ArrayList<>(N);
+    for (int i = 0; i < N; i++) {
+      patternBuffers.add(new CircularIntBuffer(i+1));
+    }
+
+    int hits = 0;
+    for (int position = 0; position < data.length; position++) {
+      int term = data[position];
+      for (int i = 0; i < patternBuffers.size(); i++) {
+        CircularIntBuffer buffer = patternBuffers.get(i);
+        buffer.push(term);
+        if (buffer.full() && matches(buffer)) {
+          handler.onPhraseMatch(position-i, i+1);
+          hits++;
+        }
+      }
+    }
+    return hits;
+  }
+
+
+  public static PhraseDetector loadFromTextFile(int N, File fp) throws IOException {
+    PhraseDetector phrases = new PhraseDetector(N);
+    Pattern spaces = Pattern.compile("\\s+");
+    try (LinesIterable lines = LinesIterable.fromFile(fp)) {
+      for (String line : lines) {
+        String[] col = spaces.split(line);
+        int n = col.length;
+        if(n > N) continue;
+        IntList pattern = new IntList(n);
+        for (String str : col) {
+          pattern.add(Integer.parseInt(str));
+        }
+        phrases.addPattern(pattern);
+      }
+    }
+    return phrases;
+  }
+
+}
