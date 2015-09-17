5cbd4f49630a623d9f3781b90dc8726b289149e4
John Foley <jfoley@cs.umass.edu>
2015-09-17 15:04:28 -0400

    tweak PhraseHitsIndexer to build the index directly; this is really slow, sadly.

--- src/main/java/edu/umass/cs/jfoley/coop/bills/PhraseHitsIndexer.java
+++ src/main/java/edu/umass/cs/jfoley/coop/bills/PhraseHitsIndexer.java
@@ -2,8 +2,10 @@ package edu.umass.cs.jfoley.coop.bills;
 
 import ciir.jfoley.chai.collections.Pair;
 import ciir.jfoley.chai.collections.list.IntList;
+import ciir.jfoley.chai.collections.util.MapFns;
 import ciir.jfoley.chai.io.Directory;
 import ciir.jfoley.chai.io.LinesIterable;
+import ciir.jfoley.chai.math.StreamingStats;
 import ciir.jfoley.chai.string.StrUtil;
 import ciir.jfoley.chai.time.Debouncer;
 import edu.umass.cs.jfoley.coop.phrases.PhraseHitsWriter;
@@ -13,6 +15,7 @@ import org.lemurproject.galago.utility.tools.Arguments;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * @author jfoley
@@ -34,6 +37,9 @@ public class PhraseHitsIndexer {
     int total = 0;
     Debouncer msg = new Debouncer();
 
+    HashMap<Integer,Integer> seenIds = new HashMap<>();
+    StreamingStats hitsPerDoc = new StreamingStats();
+
     try (
         PhraseHitsWriter writer = new PhraseHitsWriter(output, "patterns")) {
       try (LinesIterable lines = LinesIterable.fromFile(input)) {
@@ -42,20 +48,26 @@ public class PhraseHitsIndexer {
           assert (docName.startsWith("bill"));
           docName = docName.substring(4);
           int docId = docIds.get(docName);
+          MapFns.getOrInsert(seenIds, docId);
           processed++;
           if (msg.ready()) {
             System.out.println(docId + " " + docName);
-            System.out.println("rate: " + msg.estimate(processed, totalExpected));
+            System.out.println("rate: " + msg.estimate(seenIds.size(), totalExpected));
+            System.out.println("hitsPerDoc: " + hitsPerDoc);
             System.out.println("processed: " + writer.vocab.size()+" unique, "+total+" total docs: "+processed);
             System.out.println("term-rate: " + msg.estimate(total));
           }
           if (docId == docIds.getNoEntryValue()) continue;
 
           String[] items = StrUtil.takeAfter(line, "\t").replace(':', ' ').split(" ");
-          total += items.length / 2;
+          int n = items.length / 2;
+          total += n;
+          hitsPerDoc.push(n);
           for (int i = 0; i < items.length; i += 2) {
             int begin = Integer.parseInt(items[i]);
             int width = Integer.parseInt(items[i + 1]);
+
+            if(width >= 20) continue;
 
             // handle TermSlice:
             IntList slice = index.corpus.getSlice(docId, begin, width);
@@ -63,7 +75,11 @@ public class PhraseHitsIndexer {
           }
         }
       }
+      System.out.println("Done processing input");
     }
+    System.out.println("Done writing");
+
+    System.out.println("hitsPerDoc: " + hitsPerDoc);
   }
 
 }
