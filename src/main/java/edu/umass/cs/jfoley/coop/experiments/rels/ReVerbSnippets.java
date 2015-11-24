8941a6131995d22168fa930eafa51ec9eddc45b6
John Foley <jfoley@cs.umass.edu>
2015-11-24 15:29:24 -0500

    add ReVerb dependency here, rather than in another pom, oops; also add ReVerb extraction on top of snippets; quality is anecdotally low -- need to mix in document score somehow

---
+++ src/main/java/edu/umass/cs/jfoley/coop/experiments/rels/ReVerbSnippets.java
@@ -0,0 +1,123 @@
+package edu.umass.cs.jfoley.coop.experiments.rels;
+
+import ciir.jfoley.chai.collections.TopKHeap;
+import ciir.jfoley.chai.io.LinesIterable;
+import ciir.jfoley.chai.time.Debouncer;
+import edu.washington.cs.knowitall.extractor.HtmlSentenceExtractor;
+import edu.washington.cs.knowitall.extractor.ReVerbExtractor;
+import edu.washington.cs.knowitall.extractor.conf.ConfidenceFunction;
+import edu.washington.cs.knowitall.extractor.conf.ReVerbOpenNlpConfFunction;
+import edu.washington.cs.knowitall.nlp.ChunkedSentence;
+import edu.washington.cs.knowitall.nlp.OpenNlpSentenceChunker;
+import edu.washington.cs.knowitall.nlp.extraction.ChunkedBinaryExtraction;
+import org.lemurproject.galago.utility.Parameters;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author jfoley
+ */
+public class ReVerbSnippets {
+  public static class ReVerbExtraction implements Comparable<ReVerbExtraction> {
+    final double confidence;
+    final ChunkedBinaryExtraction extraction;
+
+    public ReVerbExtraction(double confidence, ChunkedBinaryExtraction extraction) {
+      this.confidence = confidence;
+      this.extraction = extraction;
+    }
+
+    public String getSubject() {
+      return extraction.getArgument1().getText();
+    }
+
+    public double getSubjectConfidence() {
+      return extraction.getArgument1().getConfidence();
+    }
+
+    public String getObject() {
+      return extraction.getArgument2().getText();
+    }
+
+    public double getObjectConfidence() {
+      return extraction.getArgument2().getConfidence();
+    }
+
+    public String getRelation() {
+      return extraction.getRelation().getText();
+    }
+
+    public Parameters toJSON() {
+      return Parameters.parseArray(
+          "confidence", confidence,
+          "subj", getSubject(),
+          "obj", getObject(),
+          "rel", getRelation());
+    }
+
+    @Override
+    public String toString() {
+      return String.format("%1.3f ``%s`` ``%s`` ``%s``", confidence, getSubject(), getRelation(), getObject());
+    }
+
+    @Override
+    public int compareTo(@Nonnull ReVerbExtraction o) {
+      return Double.compare(confidence, o.confidence);
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    //String dataset = "clue12a.sdm";
+    String dataset = "robust";
+
+    // Looks on the classpath for the default model files.
+    HtmlSentenceExtractor sentenceExtractor = new HtmlSentenceExtractor();
+    OpenNlpSentenceChunker chunker = new OpenNlpSentenceChunker();
+
+    ReVerbExtractor reverb = new ReVerbExtractor();
+
+    ConfidenceFunction confFunc = //(ignored) -> 1.0;
+        new ReVerbOpenNlpConfFunction();
+
+    int K = 50;
+    Debouncer msg = new Debouncer();
+    Map<String, TopKHeap<ReVerbExtraction>> topExtr = new TreeMap<>();
+
+    try (LinesIterable snippetLines = LinesIterable.fromFile("/mnt/scratch3/jfoley/snippets/" + dataset + ".rawsnippets.tsv.gz")) {
+      for (String snippetLine : snippetLines) {
+        String[] cols = snippetLine.split("\t");
+        String qid = cols[0];
+        String docId = cols[1];
+        String rawText = cols[4];
+        if(msg.ready()) {
+          System.err.println(qid+" "+docId);
+        }
+
+        TopKHeap<ReVerbExtraction> queryExtractions = topExtr.computeIfAbsent(qid, ignored -> new TopKHeap<>(K));
+
+        for (String sentence : sentenceExtractor.extract(rawText)) {
+          //System.err.println("\t"+sentence);
+          ChunkedSentence sent = chunker.chunkSentence(sentence);
+          for (ChunkedBinaryExtraction extr : reverb.extract(sent)) {
+            double conf = confFunc.getConf(extr);
+            queryExtractions.add(new ReVerbExtraction(conf, extr));
+          }
+        }
+        //if(snippetLines.getLineNumber()>10) break;
+      }
+
+    }
+
+    for (Map.Entry<String, TopKHeap<ReVerbExtraction>> kv : topExtr.entrySet()) {
+      String qid = kv.getKey();
+      TopKHeap<ReVerbExtraction> topExtractions = kv.getValue();
+      System.err.println(qid+": Showing the "+K+" best extractions out of "+topExtractions.getTotalSeen()+" total.");
+      for (ReVerbExtraction topExtraction : topExtractions.getSorted()) {
+        System.err.println("\t"+topExtraction.toString());
+      }
+    }
+  }
+}
---
+++ src/main/java/edu/umass/cs/jfoley/coop/experiments/rels/ScoreDocumentsForSnippets.java
@@ -0,0 +1,67 @@
+package edu.umass.cs.jfoley.coop.experiments.rels;
+
+import ciir.jfoley.chai.collections.util.MapFns;
+import ciir.jfoley.chai.io.IO;
+import ciir.jfoley.chai.io.LinesIterable;
+import ciir.jfoley.chai.time.Debouncer;
+import org.lemurproject.galago.core.parse.TagTokenizer;
+import org.lemurproject.galago.core.retrieval.LocalRetrieval;
+import org.lemurproject.galago.core.retrieval.ScoredDocument;
+import org.lemurproject.galago.core.retrieval.query.Node;
+import org.lemurproject.galago.utility.Parameters;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+/**
+ * @author jfoley
+ */
+public class ScoreDocumentsForSnippets {
+  public static void main(String[] args) throws IOException {
+    //String dataset = "clue12a.sdm";
+    String dataset = "robust";
+    LocalRetrieval ret = new LocalRetrieval("/mnt/scratch3/jfoley/"+dataset+".galago");
+
+    Map<String, String> queries = new HashMap<>();
+    for (String tsvLine : LinesIterable.fromFile("/home/jfoley/code/queries/robust04/rob04.titles.tsv").slurp()) {
+      String col[] = tsvLine.trim().split("\t");
+      queries.put(col[0], col[1]);
+    }
+
+    Map<String, Set<String>> workingSetByQuery = new TreeMap<>();
+
+    try (LinesIterable snippetLines = LinesIterable.fromFile("/mnt/scratch3/jfoley/snippets/" + dataset + ".rawsnippets.tsv.gz")) {
+      for (String snippetLine : snippetLines) {
+        String[] cols = snippetLine.split("\t");
+        String qid = cols[0];
+        String docId = cols[1];
+        MapFns.extendSetInMap(workingSetByQuery, qid, docId);
+      }
+    }
+
+    Debouncer msg = new Debouncer();
+    int i=0;
+    try (PrintWriter pw = IO.openPrintWriter("/mnt/scratch3/jfoley/snippets/"+dataset+".sdm.trecrun")) {
+      for (Map.Entry<String, Set<String>> kv : workingSetByQuery.entrySet()) {
+        String qid = kv.getKey();
+        ArrayList<String> ws = new ArrayList<>(kv.getValue());
+
+        Node sdm = new Node("sdm");
+        TagTokenizer tok = new TagTokenizer();
+        for (String term : tok.tokenize(queries.get(qid)).terms) {
+          sdm.add(Node.Text(term));
+        }
+        Parameters qp = Parameters.create();
+        qp.put("working", ws);
+
+        System.err.println("# "+qid+" " +msg.estimate(i++, workingSetByQuery.size())+" ND:"+ws.size());
+        for (ScoredDocument scoredDocument : ret.transformAndExecuteQuery(sdm, qp).scoredDocuments) {
+          pw.println(scoredDocument.toTRECformat(qid, "galago-sdm"));
+        }
+      }
+    }
+
+
+  }
+}
