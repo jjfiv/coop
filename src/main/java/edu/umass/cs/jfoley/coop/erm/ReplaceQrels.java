ee5dd2dfd54334872a5950084e4532d547fccaf4
John Foley <jfoley@cs.umass.edu>
2016-01-04 18:57:24 -0500

    point at different stuff

--- src/main/java/edu/umass/cs/jfoley/coop/erm/ReplaceQrels.java
+++ src/main/java/edu/umass/cs/jfoley/coop/erm/ReplaceQrels.java
@@ -10,6 +10,9 @@ import org.lemurproject.galago.utility.Parameters;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author jfoley
@@ -18,36 +21,49 @@ public class ReplaceQrels {
   public static void main(String[] args) throws IOException {
     Parameters argp = Parameters.parseArgs(args);
 
-    boolean clueweb = argp.get("clueweb", true);
+    for (boolean clueweb : Arrays.asList(true, false)) {
 
-    QuerySetJudgments qrels = new QuerySetJudgments(argp.get("judgments", clueweb ? "clue12.mturk.qrel" : "robust.mturk.qrel"));
-    String ranklibInput = argp.get("input", "coop/schuhmacher/text_graph/ranklib/data_clueweb/"+ (clueweb ? "clue12-sdm20-gsqueries.alldata.entities" : "rewq.alldata.entities"));
-    String ranklibOutput = argp.get("output", "coop/schuhmacher/text_graph/ranklib/data_clueweb/"+(clueweb ? "rewq-all.clue12.fixed.ranklib" : "rewq-all.robust.fixed.ranklib"));
+      QuerySetJudgments qrels = new QuerySetJudgments(argp.get("judgments", clueweb ? "clue12.mturk.qrel" : "robust.mturk.qrel"));
+      String ranklibInput = argp.get("input", "coop/schuhmacher/text_graph/ranklib/data_clueweb/" + (clueweb ? "clue12-sdm20-tfidf-gsqueries.alldata" : "rewq-tfidf.alldata"));
+      String ranklibNamedInput = argp.get("names", "coop/schuhmacher/text_graph/ranklib/data_clueweb/" + (clueweb ? "clue12-sdm20-gsqueries.alldata.entities" : "rewq.alldata.entities"));
 
-    TObjectIntHashMap<String> changesByQuery = new TObjectIntHashMap<>();
-    try (LinesIterable input = LinesIterable.fromFile(ranklibInput);
-         PrintWriter output = IO.openPrintWriter(ranklibOutput)) {
-      for (String instance : input) {
-        DenseDataPoint pt = new DenseDataPoint(instance);
-        String docName = StrUtil.takeAfter(pt.getDescription(), "#").trim();
-        String qid = pt.getID();
+      String ranklibOutput = argp.get("output", "coop/schuhmacher/text_graph/ranklib/data_clueweb/" + (clueweb ? "rewq-all.clue12.fixed.ranklib" : "rewq-all.robust.fixed.ranklib"));
 
-        //System.out.println(pt);
-        int judgment = qrels.get(qid).get(docName);
-        int oldJ = pt.getLabel()-1 > 0 ? 1 : 0;
-        //System.out.println(qid+": "+docName+" "+oldJ+" "+judgment);
-
-        if(oldJ != judgment) {
-          changesByQuery.adjustOrPutValue(qid, 1, 1);
+      List<String> names = new ArrayList<>();
+      try (LinesIterable input = LinesIterable.fromFile(ranklibNamedInput)) {
+        for (String line : input) {
+          names.add(StrUtil.takeAfter(line, "#").trim());
         }
-
-        // update judgment:
-        pt.setLabel(judgment);
-        // save to file:
-        output.println(pt);
       }
-    }
 
-    System.out.println(changesByQuery);
+
+      TObjectIntHashMap<String> changesByQuery = new TObjectIntHashMap<>();
+      try (LinesIterable input = LinesIterable.fromFile(ranklibInput);
+           PrintWriter output = IO.openPrintWriter(ranklibOutput)) {
+        for (String instance : input) {
+          if (instance.trim().isEmpty()) continue;
+          DenseDataPoint pt = new DenseDataPoint(instance);
+          String docName = names.get(input.getLineNumber() - 2); // assume parallel data :/
+          pt.setDescription("# " + docName);
+          String qid = pt.getID();
+
+          //System.out.println(pt);
+          int judgment = qrels.get(qid).get(docName);
+          int oldJ = pt.getLabel() - 1 > 0 ? 1 : 0;
+          //System.out.println(qid+": "+docName+" "+oldJ+" "+judgment);
+
+          if (oldJ != judgment) {
+            changesByQuery.adjustOrPutValue(qid, 1, 1);
+          }
+
+          // update judgment:
+          pt.setLabel(judgment);
+          // save to file:
+          output.println(pt);
+        }
+      }
+
+      System.out.println(changesByQuery);
+    }
   }
 }
